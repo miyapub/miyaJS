85213b245809d8cd88853b6f37b5940f7f3bc069
miyapub <miyapub@outlook.com>
2015-06-02 18:24:12 +0800

    ...

---
+++ app.js
@@ -0,0 +1,11 @@
+window.onload=function(){
+    /*
+    var d=document;
+    d.id=d.getElementById;
+    var canvas=d.id('canvas');
+    var c=canvas.getContext('2d');
+    c.fillStyle='#FF0000';
+    c.fillRect(0,0,80,100);
+    */
+    _(1).get
+}
